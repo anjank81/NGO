371a34df5e49090f3006d5536dbeef994c3c153b
Anjan Kumar <anjank81@gmail.com>
2021-04-25 21:08:30 +0530

    Mission section added

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default class App extends Component {
             <div className={styles.menuItem}>Home</div>
           </div>
         </div> */}
-        <Carousel>
+        <Carousel style={{ marginBottom: '15px' }}>
           <Carousel.Item>
             <img className="d-block w-100" src={image1} alt="First slide" />
             <Carousel.Caption>
